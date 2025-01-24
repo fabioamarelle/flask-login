504a8ca586688b4f6812a65f3642a46b2043a2f4
Fabio Amarelle Rodrigues <148346868+fabioamarelle@users.noreply.github.com>
2025-01-24 03:18:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.secret_key = "IcTpaep8SbYEotENW3LXQxbkNDSSZUlH" # Clave para cifrar contrase
 # Configuración MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Admin1234'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flasklogin'
 
 mysql = MySQL(app)
